1a4762969018360a2c3392e20ebd9f3171ff1547
Veronika Akimova <176243151+verakimo@users.noreply.github.com>
2026-09-18 20:47:58 +0300

    Add semantic similarity and gene ranking with tests

---
+++ src/gene_ranking.py
@@ -0,0 +1,65 @@
+from profile_similarity import symmetric_bma
+
+def score_genes(patient_phenotype_profile, gene_phenotype_profiles, ic_values):
+    """Computes symmetric BMA for patient phenotype profile and genes in gene phenotype profiles.
+
+    Args:
+        patient_phenotype_profile: Set of HPO terms describing the patient's phenotype.
+        gene_phenotype_profiles: Dictionary mapping each gene to its set of annotated HPO terms.
+        ic_values: Dictionary mapping each HPO term to its Information Content value.
+
+    Returns:
+        Dictionary mapping each gene to its symmetric BMA score.
+    """
+    gene_scores = {}
+    for gene in gene_phenotype_profiles:
+        gene_scores[gene] = symmetric_bma(
+            patient_phenotype_profile,
+            gene_phenotype_profiles[gene],
+            ic_values
+            )
+    return gene_scores
+
+
+def rank_genes(gene_scores):
+    """Ranks genes by BMA score in descending order.
+
+    Args:
+        gene_scores: BMA scores for genes.
+    
+    Returns:
+        Dictionary mapping genes to BMA scores in descending score order.
+    """
+    ranked_genes = dict(
+        sorted(
+            gene_scores.items(),
+            key=lambda item: item[1],
+            reverse=True
+            )
+        )
+    return ranked_genes
+
+
+def rich_output(ranked_genes, gene_phenotype_profiles):
+    """Creates a ranked output containing each gene's rank,
+    BMA score, and phenotype annotations.
+
+    Args:
+        ranked_genes: Sorted dictionary mapping each gene to its BMA score.
+        gene_phenotype_profiles: Dictionary mapping each gene to its set of annotated HPO terms.
+    
+    Returns:
+        List of dictionaries containing rank, gene, score, and phenotype annotations.
+    """
+    top_rank = []
+    n = 1
+    for gene in ranked_genes:
+        richer_output = {
+            "rank": n,
+            "gene": gene,
+            "score": ranked_genes[gene],
+            "phenotypes": gene_phenotype_profiles[gene]
+        }
+        top_rank.append(richer_output)
+        n += 1
+    return top_rank
---
+++ tests/test_gene_ranking.py
@@ -0,0 +1,71 @@
+import unittest
+
+from gene_ranking import score_genes, rank_genes, rich_output
+
+PATIENT_PROFILE = {"C", "B"}
+
+GENE_PROFILES = {
+    "g1": {"C"},
+    "g2": {"D", "A"},
+    "g3": {"B"}
+}
+
+IC_VALUES = {
+    'ROOT': -0.0,
+    'A': 0.5849625007211563,
+    'C': 1.5849625007211563,
+    'D': 1.5849625007211563,
+    'B': 0.5849625007211563
+    }
+
+GENE_SCORES = {
+    'g1': 1.1887218755408673,
+    'g2': 0.5849625007211563,
+    'g3': 0.4387218755408672
+}
+
+UNRANKED_GENE_SCORES = {
+    'g3': 0.4387218755408672,
+    'g1': 1.1887218755408673,
+    'g2': 0.5849625007211563
+}
+
+
+class TestGeneRanking(unittest.TestCase):
+    def test_score_genes(self):
+        actual = score_genes(PATIENT_PROFILE, GENE_PROFILES, IC_VALUES)
+        expected = {
+            'g1': 1.1887218755408673,
+            'g2': 0.5849625007211563,
+            'g3': 0.4387218755408672
+            }
+        self.assertEqual(actual, expected)
+
+    def test_rank_genes(self):
+        actual = rank_genes(UNRANKED_GENE_SCORES)
+        self.assertEqual(list(actual.keys()), ["g1", "g2", "g3"])
+
+    def test_rich_output(self):
+        actual = rich_output(GENE_SCORES, GENE_PROFILES)
+        expected = [
+            {
+                'rank': 1,
+                'gene': 'g1',
+                'score': 1.1887218755408673,
+                'phenotypes': {'C'}
+            },
+            {
+                'rank': 2,
+                'gene': 'g2',
+                'score': 0.5849625007211563,
+                'phenotypes': {'A', 'D'}
+            },
+            {
+                'rank': 3,
+                'gene': 'g3',
+                'score': 0.4387218755408672,
+                'phenotypes': {'B'}
+            }
+        ]
+        self.assertEqual(actual, expected)
+    
